117ff902c43ccb3df412be69f85829d20b050aae
Damien Luzzo <damienluzzo33@gmail.com>
2021-10-08 14:16:39 -0500

    clean up code and begin setting up google translate api

--- assets/js/matieralize.js
+++ assets/js/matieralize.js
@@ -1,21 +1,19 @@
-// event listener for auto complete input
-document.addEventListener("DOMContentLoaded", function () {
-    const inputField = document.querySelector(".autocomplete");
+var dropdown_lang = document.getElementById("dropdown");
 
-    var instances = M.Autocomplete.init(inputField, {
-        data: {
-            // null values are display icons. We can add later.
-            Google: null,
-            Yahoo: null,
-            Youtube: null,
-            OstonCode: null,
-        },
-        limit: 10,
-        minLength: 1,
-    });
+var target_language = dropdown_lang.value;
+
+fetch(`https://google-translate1.p.rapidapi.com/language/translate/v2/languages?key=AIzaSyBi6c1JBkPmzh45xH8DRXjD_6P9q0w0zaU&target=${target_language}`, {
+	"method": "GET",
+	"headers": {
+		"accept-encoding": "application/gzip",
+		"x-rapidapi-key": "96f8e26371msh5ae2fe70d5a8e2fp1b484cjsn6f1f691de9ab",
+		"x-rapidapi-host": "google-translate1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll(".sidenav");
-    var instances = M.Sidenav.init(elems);
-});
